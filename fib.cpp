6a50957db04f40d9df03ef8c7f9f6eaa0080c80d
lickmagus <u6740827@anu.edu.au>
2026-06-08 17:34:53 +0800

    init

---
+++ fib.cpp
@@ -0,0 +1,26 @@
+﻿#include <ostream>
+int fib(int n) {
+    if (n <= 1) {
+        return n;
+    }
+    int a = fib(n-1);
+    int b = fib(n-2); 
+    return a + b;
+}
+
+
+
+int main() {
+    // codes start to run from here
+
+    // some meaningless lines here for tesing
+    int one = 1;
+    int two = 2;
+    int three = one + two;
+
+    int i = 3;
+    printf("calculating fib \i\n", i);
+    int result = fib(i);
+
+    return 0;
+}
